fe39dde94e5b2087307ad6025cf167ef1985b3ba
Oleksii Kuzko <okuzk@softserveinc.com>
2019-08-16 18:12:33 +0300

    Liquibase configuration was fixed

--- src/main/resources/db/sql/v0-1-0_002.sql
+++ src/main/resources/db/sql/v0-1-0_002.sql
@@ -1,4 +1,2 @@
-ALTER TABLE public.phrase_word
+ALTER TABLE public.phrases_words
   ALTER COLUMN phrase_word_id TYPE TEXT USING phrase_word_id :: TEXT;
-ALTER TABLE public.phrase_word
-  ALTER COLUMN id DROP DEFAULT;
