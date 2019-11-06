c06c62edb46b36939a7b9f81cb3294094b071282
Oleksii Kuzko <okuzk@softserveinc.com>
2019-11-06 18:48:48 +0200

    Database tables moved to a mainapp schema

--- src/main/resources/db/sql/v0-1-0_001.sql
+++ src/main/resources/db/sql/v0-1-0_001.sql
@@ -1,85 +1,59 @@
-create table if not exists client
+create schema if not exists mainapp;
+
+create table if not exists mainapp.answer
 (
-	client_login text not null
-		constraint client_pk
-			primary key,
-	email text not null,
-	registration_date timestamp not null,
-	last_access timestamp not null,
-	is_active boolean not null,
+	client_login text,
+	question_id integer,
+	is_right boolean,
+	answered timestamp,
+	id serial not null
+);
+
+alter table mainapp.answer owner to postgres;
+
+create table if not exists mainapp.client
+(
+	client_login text,
+	email text,
+	registration_date timestamp,
+	last_access timestamp,
+	is_active boolean,
 	password integer
 );
 
-alter table client owner to postgres;
+alter table mainapp.client owner to postgres;
 
-create unique index if not exists client_user_login_uindex
-	on client (client_login);
-
-create table if not exists question
+create table if not exists mainapp.language
 (
-	id serial not null
-		constraint question_pk
-			primary key,
-	client_login text not null,
-	is_active boolean not null,
-	created timestamp not null,
-	probability_factor double precision not null,
-	probability_multiplier double precision not null,
+	language_code text,
+	name text
+);
+
+alter table mainapp.language owner to postgres;
+
+create table if not exists mainapp.question
+(
+	id serial not null,
+	client_login text,
+	is_active boolean,
+	created timestamp,
+	probability_factor double precision,
+	probability_multiplier double precision,
 	last_accessed timestamp,
 	tag text
 );
 
-alter table question owner to postgres;
+alter table mainapp.question owner to postgres;
 
-create unique index if not exists question_id_uindex
-	on question (id);
-
-create table if not exists language
+create table if not exists mainapp.word
 (
-	language_code text not null
-		constraint language_pk
-			primary key,
-	name text not null
-);
-
-alter table language owner to postgres;
-
-create unique index if not exists language_language_code_uindex
-	on language (language_code);
-
-create table if not exists word
-(
-	id serial not null
-		constraint question_word_pk
-			primary key,
-	question_id text not null,
-	word text not null,
-	added timestamp not null,
-	is_active boolean not null,
-	language_code text not null,
+	id serial not null,
+	question_id text,
+	word text,
+	added timestamp,
+	is_active boolean,
+	language_code text,
 	transcription text
 );
 
-alter table word owner to postgres;
-
-create unique index if not exists question_word_id_uindex
-	on word (id);
-
-create table if not exists answer
-(
-	client_login text not null,
-	question_id integer not null,
-	is_right boolean not null,
-	answered timestamp not null,
-	id serial not null
-		constraint answer_pk
-			primary key
-);
-
-alter table answer owner to postgres;
-
-create unique index if not exists answer_client_login_uindex
-	on answer (client_login);
-
-create unique index if not exists answer_id_uindex
-	on answer (id);
+alter table mainapp.word owner to postgres;
